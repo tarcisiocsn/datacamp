5fc41b72e83ec4b9f5450cc0b423ecca16992faf
Tarcísio Costa <68601128+tarcisiocsn@users.noreply.github.com>
2021-05-06 14:48:13 -0300

    Update data_manip_pandas.py

--- data_manip_pandas.py
+++ data_manip_pandas.py
@@ -102,7 +102,7 @@ min        434.000          75.000  5.776e+05
 50%       3082.000        1482.000  4.461e+06
 75%       6781.500        3196.000  7.341e+06
 max     109008.000       52070.000  3.946e+07
-
+ 
  
 # FIRST CHAPTER - 1.1 Sorting and Subsetting
  #abordaremos os dois mais simples e possivelmente maneiras mais importantes de encontrar partes interessantes do seu DataFrame 
@@ -115,8 +115,68 @@ max     109008.000       52070.000  3.946e+07
  # with we setting the ascending argument to False will sort the data the other way around, from heaviest dog to lightest dog 
  dog.sort_values('weight_kg', ascending = False)
  
- #
+ #Sorting by multiples variables
+ dogs.sort_values(['weight_kg', 'height_cm']) 
+ 
+ # to change the direction values are sorted in, pass a list ascending argument to specify wich direction sorting should be done for each variable
+ dogs.sort_values(['weight_kg', 'height_cm'], ascending = [ True, False]) # example 
+ 
+ # subsetting columns (just zoom in on just one column)
+ dogs['name'] 
+ 
+ # subsetting multiple columns -> to select it, you need two pairs of square brackets
+ dogs[['breed', 'height_cm']]
+ 
+ # PS. lembrando que tem 2 brackets, pq o de fora corresponde ao subsetting do DataFrame, mas o de brackets de dentro é que criam a lista de nomes de colunas que iremos subsetar. 
+ 
+ # Subsetting rows -> the most common way to subset it, is by creating a logical condition to filter against.
+ #- for exemple -> let's find all the dogs whose height is greater than centimeters. 
+ dogs['height_cm'] > 50  
+ 
+ # output -> será um dataframe de boolens verificando quem é true e quem não é (True or False)
+ 
 
+ # We can use the the logical condition inside of square brackets to subset the rows we're interested in to get all of the dogs taller than 50 cm. 
+ # dará um dataframe topado com tudo que tem direito dos cachorros 
+ dogs[dogs['height_cm'] > 50]
+ 
+ # Subsetting rows based on text data 
+ dogs[dogs['breed'] == 'Labrador'] # to filter the dogs that are Labradors
+ 
 
+# subsetting rows based on dates
+dogs[dogs['date_of_bith'] > '2015-01-01'] # só os animais que nasceram depois dessa data
+# Subsetting rows based on multiple conditions
+is_lab = [dogs['breed'] == 'Labrador'
+is_brown = dogs['color'] == 'Brown'
+dogs[is_lab & is_brown] # compilou os 2 e dará o cachorro que é labrador e marrom e toda as informações dele
 
+           
+# Subsetting using .isin() 
+is_black_or_brown = dogs['color'].isin(['Black' , 'Brown'])
+dogs[is_black_or_brown] # ai dará apenas cachorros com cor preta e marrom
 
+ # exemples
+Sort homelessness by the number of homeless individuals, from smallest to largest, and save this as homelessness_ind.
+Print the head of the sorted DataFrame.
+
+#sort homelessness by individual (vai pegar os lugares com os menores individuos
+homelessness_ind = homelessness.sort_values('individuals') 
+#print the top few rows
+print(homelessness_ind.head())
+
+          #output
+                 region         state  individuals  family_members  state_pop
+50            Mountain       Wyoming        434.0           205.0     577601
+34  West North Central  North Dakota        467.0            75.0     758080
+7       South Atlantic      Delaware        708.0           374.0     965479
+39         New England  Rhode Island        747.0           354.0    1058287
+45         New England       Vermont        780.0           511.0     624358
+          
+          
+
+          
+                    
+           
+           
+            
