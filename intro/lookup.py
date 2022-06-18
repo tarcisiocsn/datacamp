8a6272abd1386362c6b6e680bb17a743aa1c58de
Tarcísio Costa <68601128+tarcisiocsn@users.noreply.github.com>
2022-06-17 21:56:08 -0400

    Create lookup.py

---
+++ intro/lookup.py
@@ -0,0 +1,8 @@
+# lookup function example
+plate = 'FQR***' # placa do carro que só sabemos as 3 primeiras letras
+lookup_plate(plate) # lookup() function
+# Call lookup_plate() with the keyword argument for color
+lookup_plate(plate, color='Green')  #lookup pode ter outro argumento, neste caso é a placa 'plate' e a cor do carro especifico Green
+
+
+
